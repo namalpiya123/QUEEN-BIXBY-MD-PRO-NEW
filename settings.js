07de237519df2d332a382bf1cff4d67c1c71a987
namalpiya123 <133758133+namalpiya123@users.noreply.github.com>
2023-07-23 15:12:30 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94711421243']
-global.premium = ['94711421243']
-global.ownernomer = '94711421243'
+global.owner = ['94753721749']
+global.premium = ['94753721749']
+global.ownernomer = '94753721749'
 global.ownername = 'Dark Alpha'
-global.botname = 'BIXBY-MD USER BOT'
+global.botname = 'RASIYA-MD USER BOT'
 global.footer = ' Qᴜᴇᴇɴ ʙɪxʙʏ ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ.'
 global.ig = 'https://github.com/darkalphaxteam'
 global.region = 'India, South India, Kerala'
@@ -31,7 +31,7 @@ global.sc = 'https://github.com/darkalphaxteam/QUEEN-BIXBY-MD-PRO-NEW'
 global.myweb = 'https://youtube.com/channel/UCvpyOCBCaBSCdv4QolnO0WQ'
 global.packname = 'Queen Bixby MD'
 global.author = 'DARK ALPHA'
-global.sessionName = 'session'
+global.sessionName = 'NEE3OHBCUEI='
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '🚢'
 global.mess = {
